66be620b10382736db83f30fac91ab960c6409b8
Tova Meystel <mystel09@gmail.com>
2015-07-02 10:01:42 -0400

    added pod files

---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, '8.0'
+
+target 'Ghost' do
+	pod 'Parse'
+	pod 'ParseUI'
+	pod 'FBSDKCoreKit'
+	pod 'ParseFacebookUtils'
+end
+
+
---
+++ Pods/Target Support Files/Pods-Ghost-ParseFacebookUtils/Pods-Ghost-ParseFacebookUtils-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Ghost_ParseFacebookUtils : NSObject
+@end
+@implementation PodsDummy_Pods_Ghost_ParseFacebookUtils
+@end
