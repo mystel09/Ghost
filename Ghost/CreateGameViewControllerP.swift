f11ea6aff2a0b41620bbff2b8a300ad9425e22fc
Tova Meystel <mystel09@gmail.com>
2015-07-27 17:03:46 -0400

    added game center control of choosing friends, need to add functions to handle requests

--- Ghost/CreateGameViewControllerP.swift
+++ Ghost/CreateGameViewControllerP.swift
@@ -15,7 +15,7 @@
 import UIKit
 import GameKit
 
-class CreateGameViewControllerP: UITableViewController, GKGameCenterControllerDelegate{
+class CreateGameViewControllerP: UITableViewController, GKGameCenterControllerDelegate {
     
     var colors: [UIColor] = [Colors.Red,Colors.Purple, Colors.Yellow, Colors.Pink,Colors.Orange,Colors.Green,Colors.Brown,Colors.Blue]
     var newGame: GameP = GameP()
@@ -27,77 +27,28 @@ class CreateGameViewControllerP: UITableViewController, GKGameCenterControllerDe
     struct Constants {
         static let maxNumberOfPlayers = 4
         static let minNumberOfPlayers = 2
-        static let PlayersHeaderIdentifier = "PlayersHeader"
+        static let GamessHeaderIdentifier = "GamesHeader"
+        static let GamessCellIdentifier = "GamesCell"
         static let WordSizeCellIdentifier = "WordSize"
         static let StartButtonCellIdentifier = "StartButton"
-        static let AddPlayerCellIdentifier = "AddPlayer"
-        static let PlayerCellIdentifier = "Player"
-        static let SettingsIdentifier = "SettingsHeader"
+        static let SettingsIdentifier = "Settings"
         static let StartGameSegue = "StartOnlineGameSegue"
-        static let FindFriendsIdentifier = "FindFriends"
         static let WordSizeTag = 100
     }
     
     override func viewDidAppear(animated: Bool) {
-       // var players = PFUser.currentUser()!["playedWith"] as? [PFUser]
-        // query.findObjectsInBackgroundWithBlock { ( results: [AnyObject]?, error: NSError?) -> Void in
-        //                if error != nil {
-        //                    println("error")
-        //                    return
-        //                }
-        //                let results = results as? [PFObject] ?? []
-//        if  players?.count > 0 {
-//        for player in players! { //getting all newly added players
-//            self.newGame.playersP.append(PlayerP(name: player["username"]! as! String, playerColor: self.colors.count-1))
-//            self.colors.removeLast() //remove that from array, while using the last color
-//            self.currentNumberOfPlayers++
-//        }
-//        tableView.reloadData()
-//        }
+    
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         //TODO:
         self.authenticateLocalPlayer()
-
-        self.newGame.playersP.append(PlayerP(name: "", playerColor: colors.count-1))
-//        self.colors.removeLast() //remove that from array, while using the last color
 //        self.newGame.playersP.append(PlayerP(name: "", playerColor: colors.count-1))
-//        self.colors.removeLast()
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "greybrick.gif")!) //background color
+        tableView.delegate = self
     }
-    
-    @IBAction func AddPlayer(sender: AnyObject) { //finding fellow users through parse
-//        var query = PFQuery(className:"playedWith")
-//        
-//        query.findObjectsInBackgroundWithBlock { ( results: [AnyObject]?, error: NSError?) -> Void in
-//            if error != nil {
-//                println("error")
-//                return
-//            }
-//                let results = results as? [PFObject] ?? []
-//                
-//                for player in results { //getting all newly added players
-//                    self.newGame.playersP.append(PlayerP(name: player["username"]! as! String, playerColor: self.colors.count-1))
-//                    self.colors.removeLast() //remove that from array, while using the last color
-//                    self.currentNumberOfPlayers++
-//                }
-//            self.tableView.reloadData()
-//
-//            }
-         }
-    @IBAction func unwindToSegue(segue: UIStoryboardSegue) {
-        if segue.identifier == "Save" {
-//                            for player in players { //getting all newly added players
-//                    self.newGame.playersP.append(PlayerP(name: player["username"]! as! String, playerColor: self.colors.count-1))
-//                    self.colors.removeLast() //remove that from array, while using the last color
-//                    self.currentNumberOfPlayers++
-//                }
-                self.tableView.reloadData()
-                
-        }
-    }
-    
+            
+        
     @IBAction func StartGame(sender: AnyObject) {
         //check if at least one other player
         
@@ -106,18 +57,11 @@ class CreateGameViewControllerP: UITableViewController, GKGameCenterControllerDe
     }
     // saving an instance of the game to use in the next controller
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if (segue.identifier == "StartOnlineGameSegue") {
+        if (segue.identifier == Constants.StartGameSegue) {
             //game
             if let navVC = segue.destinationViewController as? UINavigationController {
                 let CreateGameViewControllerP = navVC.topViewController as! GameViewControllerP
                 CreateGameViewControllerP.currentGame = newGame
-//                //newGame.saveInBackgroundWithBlock{(success: Bool, error: NSError?) -> Void in
-//                
-//                    if success == true {
-//                        println("game saved")
-//                        return
-//                    }
-//                  }
                }
             }
         }
@@ -129,57 +73,85 @@ class CreateGameViewControllerP: UITableViewController, GKGameCenterControllerDe
         alertView.addButtonWithTitle("OK")
         alertView.show()
     }
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        if indexPath.section == 1 {
+            getPlayers()
+        }
+    }
     
+    func getPlayers() {
+        var request: GKMatchRequest = GKMatchRequest()
+        var gamecontrol: GKTurnBasedMatchmakerViewController = GKTurnBasedMatchmakerViewController(matchRequest: request)
+        
+        request.minPlayers = 2
+        request.maxPlayers = 4
+        
+        gamecontrol.turnBasedMatchmakerDelegate = self
+        //        gamecontrol.showExistingMatches
+        
+        self.presentViewController( gamecontrol, animated: true, completion: nil)
+        
+        
+
+    }
     // MARK: - Table view data source
     
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 2
+        return 3
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if section == 0 {
-            if self.newGame.playersP.count == Constants.maxNumberOfPlayers {
-                return self.newGame.playersP.count
-            }
-            else {
-                return self.newGame.playersP.count+1
-            }
+            return 1
         }
+
         else {
-            return 2
+            return 1
         }
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         if indexPath.section == 0 {
-            if self.newGame.playersP.count != Constants.maxNumberOfPlayers && indexPath.row == self.tableView(self.tableView, numberOfRowsInSection: 0)-1 {
-                let cell = tableView.dequeueReusableCellWithIdentifier(Constants.AddPlayerCellIdentifier, forIndexPath: indexPath) as! UITableViewCell
-                return cell
-            }
             
-            let cell = tableView.dequeueReusableCellWithIdentifier(Constants.PlayerCellIdentifier, forIndexPath: indexPath) as! PlayerViewCell
-            cell.playerP = self.newGame.playersP[indexPath.row]
-            //cell.nameField.delegate = self
-            cell.nameField.tag = indexPath.row
-
+            let cell = tableView.dequeueReusableCellWithIdentifier(Constants.GamessCellIdentifier, forIndexPath: indexPath) as! UITableViewCell
+                return cell
+        }
+        else if indexPath.section == 1 {
+                let cell = tableView.dequeueReusableCellWithIdentifier(Constants.StartButtonCellIdentifier, forIndexPath: indexPath) as! UITableViewCell
             return cell
         }
         else {
-                let cell = tableView.dequeueReusableCellWithIdentifier(Constants.StartButtonCellIdentifier, forIndexPath: indexPath) as! UITableViewCell
+        
+            let cell = tableView.dequeueReusableCellWithIdentifier(Constants.SettingsIdentifier, forIndexPath: indexPath) as! UITableViewCell
             return cell
-                
-            
+ 
         }
     }
         override func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let identifier = section == 0 ? Constants.PlayersHeaderIdentifier : Constants.SettingsIdentifier
-        let headerCell = tableView.dequeueReusableCellWithIdentifier(identifier) as! UITableViewCell
-        //headerCell.backgroundColor = UIColor.lightGrayColor()
+            if section == 0 {
+                
+            let identifier = Constants.GamessHeaderIdentifier
+            
+        let headerCell = tableView.dequeueReusableCellWithIdentifier(identifier) as! UIView!
         return headerCell
+        }
+        return nil
+            
     }
-    
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        if indexPath.section == 1 || indexPath.section == 2 {
+            return 64.0
+        }
+        return 44.0
+    }
     override func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 64.0
+        if section == 0 {
+            return 64.0
+        }
+        else {
+            
+        return 0
+        }
     }
     
     func authenticateLocalPlayer() {
@@ -232,12 +204,16 @@ extension CreateGameViewController: GKMatchDelegate {
             return self.expectedPlayerCount
         }
     }
+    
     func match(match: GKMatch!, didReceiveData data: NSData!, fromRemotePlayer player: GKPlayer!) {
         
     }
-    func match(match: GKMatch!, didReceiveData data: NSData!, fromPlayer playerID: String!) {
+    
+    func match(theMatch: GKMatch!, didReceiveData data: NSData!, fromPlayer playerID: String!) {
+        
         
     }
+    
     // The player state changed (eg. connected or disconnected)
     func match(match: GKMatch!, player: GKPlayer!, didChangeConnectionState state: GKPlayerConnectionState) {
         
@@ -246,12 +222,40 @@ extension CreateGameViewController: GKMatchDelegate {
     func match(match: GKMatch!, didFailWithError error: NSError!) {
         
     }
+    
     func match(match: GKMatch!, shouldReinviteDisconnectedPlayer player: GKPlayer!) -> Bool {
         return true
     }
+    
     func match(match: GKMatch!, shouldReinvitePlayer playerID: String!) -> Bool {
         return true
     }
-   
+    func hideMatchmaker() {
+        if self.matchMakerController {
+            self.matchMakerController.delegate = nil
+            self.matchMakerController.dismissViewControllerAnimated(true, completion: nil)
+            self.matchMakerController = nil
+            }
+    }
     
+   
+}
+extension CreateGameViewControllerP: GKTurnBasedMatchmakerViewControllerDelegate {
+    
+    func turnBasedMatchmakerViewControllerWasCancelled(viewController: GKTurnBasedMatchmakerViewController!) {
+        self.hideMatchMaker()
+    }
+    
+    func turnBasedMatchmakerViewController(viewController: GKTurnBasedMatchmakerViewController!, didFailWithError error: NSError!) {
+        return
+    }
+    
+    func turnBasedMatchmakerViewController(viewController: GKTurnBasedMatchmakerViewController!, playerQuitForMatch match: GKTurnBasedMatch!) {
+       
+    }
+    
+    func turnBasedMatchmakerViewController(viewController: GKTurnBasedMatchmakerViewController!, didFindMatch match: GKTurnBasedMatch!) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+        self.performSegueWithIdentifier(Constants.StartGameSegue, sender: match)
+    }
 }
